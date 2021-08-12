3af1f9ab3620f6f11a88c988a5fb8c8ca3ae6690
Ashwin Padaki <apadaki22@gmail.com>
2021-08-12 15:32:06 -0400

    documented data formatting code, clarified origin_time/rigidity, and cleaned up imports

--- format_gauge.py
+++ format_gauge.py
@@ -1,10 +1,14 @@
+"""Script for formatting NOAA gauge data for the Chile 2015 earthquake"""
 def format_gauges_chile2015(gaugeinfo):
     import pandas as pd
     for gaugeno, offset in gaugeinfo:
         df = pd.read_csv('dart{}.txt'.format(gaugeno), delim_whitespace=True)
         df.columns = ['time', 'year', 'month', 'day', 'hours', 'minutes', 'seconds', 'raw_obs', 'fitted_tidal', 'residual', 'blank']
+        # only store time and residual data
         df = df[['time', 'residual']]
+        # earthquake origin time (from USGS): 2015-09-16 22:54:32.860 (UTC)
         origin_time = 259 + (22 * 60**2 + 54*60 + 32.860)/(24 * 60**2)
         df.time = df.time.map(lambda time : 24*60**2 * (time-origin_time))
+        # option to include manual offsets to correct for equilibria that are not at sea level
         df.residual = df.residual.map(lambda res : res + offset)
         df.to_csv('{}_notide.txt'.format(gaugeno), index=False, header=False, sep=' ')
--- get_dtopo_csv.py
+++ get_dtopo_csv.py
@@ -1,6 +1,7 @@
+"""Script for formatting USGS finite fault data into a CSV file readable by GeoClaw's dtopotools module"""
 def get_csv(url, param_fname, csv_fname):
     import clawpack.clawutil.data
-    import requests, os    
+    import os    
     try:
         CLAW = os.environ['CLAW']
     except:
@@ -11,8 +12,11 @@ def get_csv(url, param_fname, csv_fname):
     import pandas as pd
     df = pd.read_csv(os.path.join(scratch_dir,param_fname), skiprows=9, delim_whitespace=True)
     df.columns = ['latitude', 'longitude', 'depth', 'slip', 'rake', 'strike', 'dip', 'rupture_time', 'rise_time', 'fall_time', 'mo']
+    # length/width can be automated, but for convenience, they're manually written here
     df['length'] = 12.00
     df['width'] = 8.80
-    df['mu'] = df['mo'] / (10**8 * df['length'] * df['width'] * df['slip']) # km*km*cm == 10**4 m^3
+    # rigidity = (moment seismicity) / (slip * area)
+    # cm * km * km = 10^4 m^3
+    df['mu'] = df['mo'] / (10**4 * df['length'] * df['width'] * df['slip']) 
     df.drop(['fall_time', 'mo'], axis=1, inplace=True)
     df.to_csv(csv_fname, index=False)
